bc0feff4f48b06392a9572933af0a050c71fe66d
Princeling <2230647190@qq.com>
2019-02-18 00:11:59 +0800

    回忆了一下回溯算法，话说数据结构之前一定要学离散数学?我不信

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>      // standard input output
+
+int main(int argc, char const *argv[])
+{
+    int i = 8;
+    printf("%d,%d,%d,%d",i++,++i,--i,i--);
+
+    // print 打印 format 格式化
+    return 0;
+}
